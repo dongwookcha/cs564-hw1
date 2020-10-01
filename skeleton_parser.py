2e5b6631b9a82b4e8b3db3e24dee5dabbdfa4baf
dongwook <dongwookcha0@gmail.com>
2020-10-01 11:52:35 -0500

    Update skeleton_parser and unzip json files

--- skeleton_parser.py
+++ skeleton_parser.py
@@ -27,6 +27,7 @@ Happy parsing!
 import sys
 from json import loads
 from re import sub
+import os
 
 columnSeparator = "|"
 
@@ -34,6 +35,16 @@ columnSeparator = "|"
 MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',\
         'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
+users = {}
+itemcollection = set()
+bidCollection = set()
+catBidCollection = set()
+usersCollection  = set()
+jsons = set()
+debug3Counter = 0
+debug4 = 0
+debug5 = None
+debug6 = set()
 """
 Returns true if a file ends in .json
 """
@@ -68,6 +79,123 @@ def transformDollar(money):
         return money
     return sub(r'[^\d.]', '', money)
 
+
+def userInfo(item):
+    sell = item['Seller']
+    buy = item['Bids']
+    loc = item['Location']
+    country = item['Country']
+    if buy == None:
+        pass
+    else:
+        #debug = buy[0]
+        for aBid in buy:
+            abuy = aBid['Bid']['Bidder']
+            if abuy['UserID'] in users:
+                users[abuy['UserID']]['buyer'] = True
+            else:
+                tempDic = {}
+                tempDic['UserID'] = abuy['UserID']
+                tempDic['Rating'] = abuy['Rating']
+                tempDic['Location'] = abuy['Location'] if "Location" in abuy else 'NULL'
+                tempDic['Country'] = abuy['Country'] if "Country" in abuy else 'NULL'
+                tempDic['seller'] = False
+                tempDic['buyer'] = True
+                users[abuy['UserID']] = tempDic
+    if sell == None:
+        pass
+    else:
+        if sell['UserID'] in users:
+            users[sell['UserID']]['seller'] = True
+        else:
+            tempDic = {}
+            tempDic['UserID'] = sell['UserID']
+            tempDic['Rating'] = sell['Rating']
+            tempDic['Location'] = loc
+            tempDic['Country'] = country
+            tempDic['seller'] = True
+            tempDic['buyer'] = False
+            users[sell['UserID']] = tempDic
+
+def itemInfo(item):
+    global debug3Counter
+    global debug4,debug5
+    item_id = item['ItemID'].strip()
+    name = item['Name'].strip()
+    tempcatitem = set()
+    for aCatt in item['Category']:
+        tempcatitem.add(aCatt)
+    categoryNum = str(len(tempcatitem))
+    #categoryNum = str(len(item['Category']))
+    if categoryNum == '4':
+        debug3Counter = debug3Counter + 1
+    currently = transformDollar(item['Currently'])
+    if float(currently) > debug4:
+        debug4 = float(currently)
+        debug5 = item_id
+    firstBid = transformDollar(item['First_Bid'])
+    numBid = item['Number_of_Bids'].strip()
+    loc = item['Location'].strip()
+    country = item['Country'].strip()
+    started = transformDttm(item['Started']).strip()
+    ends = transformDttm(item['Ends']).strip()
+    seller = item['Seller']['UserID'].strip()
+    if "Buy_Price" in item:
+        buy_price = transformDollar(item['Buy_Price'])
+    else:
+        buy_price = 'NULL'
+
+    aSequence = (item_id,name,categoryNum,currently,firstBid,numBid,loc,country,started,ends,seller,buy_price)
+    combine = "|".join( aSequence )
+    combine = combine + '\n'
+    itemcollection.add(combine)
+
+def bidInfo(item):
+    buy = item['Bids']
+    if buy == None:
+        pass
+    else:
+        for aBid in buy:
+            tempItemID = item['ItemID']
+            abuy = aBid['Bid']['Bidder']
+            tempID = abuy['UserID']
+            tempRating = abuy['Rating']
+            tempLocation = abuy['Location'] if "Location" in abuy else 'NULL'
+            tempCountry = abuy['Country'] if "Country" in abuy else 'NULL'
+            tempTime = transformDttm(aBid['Bid']['Time'])
+            tempAmount = transformDollar(aBid['Bid']['Amount'])
+            aSequence = (tempItemID,tempID,tempRating,tempLocation,tempCountry,tempTime,tempAmount)
+            combine = "|".join( aSequence )
+            combine = combine + '\n'
+            bidCollection.add(combine)
+
+def catBidInfo(item):
+    cats = item['Category']
+    buy = item['Bids']
+    if buy == None:
+        pass
+    else:
+        for aBid in buy:
+            tempAmount = transformDollar(aBid['Bid']['Amount'])
+            #debug
+            if float(tempAmount) >  100:
+                for aCateg in cats:
+                    debug6.add(aCateg)
+            # debug end
+            abuy = aBid['Bid']['Bidder']
+            tempID = abuy['UserID']
+            for aCateg in cats:
+                aSeq = (aCateg,tempAmount,tempID)
+                combine = "|".join(aSeq)
+                combine = combine + '\n'
+                catBidCollection.add(combine)
+
+
+
+
+
+
+
 """
 Parses a single json file. Currently, there's a loop that iterates over each
 item in the data set. Your job is to extend this functionality to create all
@@ -77,17 +205,54 @@ def parseJson(json_file):
     with open(json_file, 'r') as f:
         items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json file
         for item in items:
-            """
-            TODO: traverse the items dictionary to extract information from the
-            given `json_file' and generate the necessary .dat files to generate
-            the SQL tables based on your relation design
-            """
-            pass
+            userInfo(item)
+            itemInfo(item)
+            bidInfo(item)
+            catBidInfo(item)
+
+def reformUser():
+    for (key,value) in users.items():
+        tempId = value['UserID']
+        tempRating = value['Rating']
+        tempLocation = value['Location']
+        tempCoutry = value['Country']
+        tempSeller = str(value['seller'])
+        tempbuyer = str(value['buyer'])
+        seq = (tempId,tempRating,tempLocation,tempCoutry,tempSeller,tempbuyer)
+        combine = "|".join(seq)
+        combine = combine + '\n'
+        usersCollection.add(combine)
+
+def generateDAT():
+    with open("itemcollection.dat", "w") as f:
+        for aString in itemcollection:
+            f.write(aString)
+    #bidCollection
+    with open("bidCollection.dat", "w") as f:
+        for aString in bidCollection:
+            f.write(aString)
+    #catBidCollection
+    with open("catBidCollection.dat", "w") as f:
+        for aString in catBidCollection:
+            f.write(aString)
+    #usersCollection
+    with open("usersCollection.dat", "w") as f:
+        for aString in usersCollection:
+            f.write(aString)
+
+
+
+
+
+
+
+
 
 """
 Loops through each json files provided on the command line and passes each file
 to the parser
 """
+'''
 def main(argv):
     if len(argv) < 2:
         print >> sys.stderr, 'Usage: python skeleton_json_parser.py <path to json files>'
@@ -96,7 +261,32 @@ def main(argv):
     for f in argv[1:]:
         if isJson(f):
             parseJson(f)
-            print "Success parsing " + f
+            print ("Success parsing " + f)
+    
 
 if __name__ == '__main__':
     main(sys.argv)
+    '''
+
+
+#parseJson("items-0.json")
+
+
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            yield f
+
+
+base = '.'
+debug = findAllFile(base)
+print()
+for i in findAllFile(base):
+    if isJson(i):
+        jsons.add(i)
+for aJson in jsons:
+    parseJson(aJson)
+    #print(aJson)
+reformUser()
+generateDAT()
+
