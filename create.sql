d8ceacff2057ea43e57607191c4870a3057b5565
dongwook <dongwookcha0@gmail.com>
2020-10-03 05:56:46 -0500

    update part d

--- create.sql
+++ create.sql
@@ -1,50 +1,76 @@
-drop table if exists Items;
-drop table if exists Categories;
-drop table if exists ItemCategories;
-drop table if exists Bids;
-drop table if exists Users;
+drop table if exists userInfo;
+drop table if exists itemInfo;
+drop table if exists bidInfo;
+drop table if exists catBidInfo;
 
-create table Items (
-ItemID		INT	 	NOT NULL UNIQUE,
-Description VARCHAR(255)	NOT NULL,
-Started		DATE		NOT NULL,
-Ended		DATE		NOT NULL,
-Currently	DOUBLE,
-Buy_Price	DOUBLE,
-First_Bid	DOUBLE,
-Number_of_Bids INT,
-PRIMARY KEY (ItemID)
-);
 
-create table Categories (
-CategoryID	INT		NOT NULL UNIQUE,
-Name		VARCHAR(255)	NOT NULL,
-PRIMARY KEY (CategoryID)
-);
-
-create table ItemCategories (
-ItemID		INT		NOT NULL UNIQUE,
-CategoryID	INT		NOT NULL UNIQUE,
-PRIMARY KEY (ItemID, CategoryID),
-FOREIGN KEY (ItemID) REFERENCES Item (ItemID),
-FOREIGN KEY (CategoryID) REFERENCES Category (CategoryID)
-);
-
-create table Bids (
-BidID	INT		NOT NULL UNIQUE,
-Time	DATE		NOT NULL,
-Amount	DOUBLE		NOT NULL,
-PRIMARY KEY (BidID),
-FOREIGN KEY BidID REFERENCES Item (ItemID),
-FOREIGN KEY BidID REFERENCES User (UserID)
-);
-
-create table Users (
+create table userInfo (
 UserID		INT		NOT NULL UNIQUE,
 Rating		INT		NOT NULL,
 Location	VARCHAR(255)	NOT NULL,
 Country	    VARCHAR(255)	NOT NULL,
+isSeller    BOOL         NOT NULL,
+isBuyer     BOOL         NOT NULL,
 PRIMARY KEY (UserID),
-FOREIGN KEY UserID REFERENCES Item (ItemID),
-FOREIGN KEY UserID REFERENCES Bid (BidID)
+FOREIGN KEY UserID REFERENCES bidInfo (BidID)
 );
+
+create table itemInfo (
+ItemID INT NOT NULL UNIQUE,
+Name VARCHAR(255) NOT NULL,
+CategoryNum INT NOT NULL,
+Currently DOUBLE,
+FirstBid DOUBLE,
+NumberBids INT,
+Location VARCHAR(255) NOT NULL
+Country VARCHAR(255) NOT NULL
+Started DATE NOT NULL,
+Ended DATE NOT NULL,
+Seller VARCHAR(225) NOT NULL,
+Buy_Price DOUBLE,
+FOREIGN KEY Seller REFERENCES userInfo(UserID),
+PRIMARY KEY (ItemID)
+);
+
+create table bidInfo (
+ItemID INT NOT NULL UNIQUE,
+UserID INT NOT NULL UNIQUE,
+Location VARCHAR(225) NOT NULL,
+Country VARCHAR(225) NOT NULL,
+Time DATE NOT NULL,
+Amount DOUBLE NOT NULL,
+PRIMARY KEY (ItemID),
+FOREIGN KEY ItemID REFERENCES itemInfo (ItemID),
+FOREIGN KEY UserID REFERENCES userInfo (UserID)
+);
+
+create table catBidInfo (
+aCateg     VARCHAR(225) NOT NULL,
+Amount     INT          NOT NULL UNIQUE,
+UserID     INT          NOT NULL UNIQUE
+);
+
+--create table Categories (
+--CategoryID	INT		NOT NULL UNIQUE,
+--Name		VARCHAR(255)	NOT NULL,
+--PRIMARY KEY (CategoryID)
+--);
+--
+--create table ItemCategories (
+--ItemID		INT		NOT NULL UNIQUE,
+--CategoryID	INT		NOT NULL UNIQUE,
+--PRIMARY KEY (ItemID, CategoryID),
+--FOREIGN KEY (ItemID) REFERENCES Items (ItemID),
+--FOREIGN KEY (CategoryID) REFERENCES Categories (CategoryID)
+--);
+
+--create table Bids (
+--BidID	INT		NOT NULL UNIQUE,
+--Time	DATE		NOT NULL,
+--Amount	DOUBLE		NOT NULL,
+--PRIMARY KEY (BidID),
+--FOREIGN KEY BidID REFERENCES Items (ItemID),
+--FOREIGN KEY BidID REFERENCES Users (UserID)
+--);
+
+
